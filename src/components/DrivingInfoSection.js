29221f72c3abdae776d33e8cddb5c7d420c09ef0
Ori Gold <orig@ottopia.tech>
2019-08-16 01:29:42 +0300

    added more content to driving pages

--- src/components/DrivingInfoSection.js
+++ src/components/DrivingInfoSection.js
@@ -2,11 +2,18 @@ import React from "react";
 import { PageHeader } from "antd";
 
 const DrivingInfoSection = props => {
+	const yearsLeft = 2;
+	const eligibiltyStatus = "oleh";
+
 	return (
 		<div className="section driving-info-section">
 			<PageHeader title={<h3>Driving Information</h3>}/>
 			<div>
-				<p className="">Lorem ipsum and shit...</p>
+				<p className="">
+					As an {eligibiltyStatus}, you have {yearsLeft} years
+					left to take advantage of purchasing a new car in Israel or importing one
+					from abroad.
+				</p>
 			</div>
 		</div>
 	)
