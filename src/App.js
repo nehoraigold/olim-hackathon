f916d5151249cfe7b868025b81c7c086b9187416
Ori Gold <orig@ottopia.tech>
2019-08-15 21:39:32 +0300

    basic skeleton start

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Router, Route, Switch } from "react-router-dom";
+import DrivingPage from "./containers/DrivingPage";
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Router>
+			<div className="App">
+				<Navbar/>
+				<Switch>
+					<Route path="/" component={LandingPage}/>
+					<Route path="/driving" component={DrivingPage}/>
+				</Switch>
+			</div>
+		</Router>
+	);
 }
 
 export default App;
---
+++ src/containers/DrivingPage.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const DrivingPage = props => {
+	return (
+		<div>
+			<DrivingInfoSection/>
+			<BenefitsSection>
+				<LicenseTransferSubsection/>
+				<BuyingCarSubsection/>
+			</BenefitsSection>
+		</div>
+	)
+};
+
+export default DrivingPage;
