077608e4be9499368cb710c08e962fc0061477b2
Erani <ezra.erani@ironsrc.com>
2019-08-16 04:45:51 +0300

    methods filtering benefits

--- src/database/mockdata/content.js
+++ src/database/mockdata/content.js
@@ -1,3 +1,10 @@
-export const CONTENT = {
+require('./benefits');
 
+export const KEY_CATEGORY_DEFAULT = "category_default";
+export const KEY_CATEGORY_DRIVING = "category_driving";
+
+export const CONTENT = {
+    "category_driving": {
+        id:"category_driving"
+    }
 };
